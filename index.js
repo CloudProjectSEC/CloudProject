9ac13c004d0c936820d2f7d669dc73395cab3407
Andre Hideo <onoda.hideo@gmail.com>
2025-04-01 23:33:01 -0400

    New test secret

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ dotenv.config()
 console.log('It works!');
 console.log('New feature is logging');
 console.log(`Using API_KEY \"${process.env.API_KEY}\"`)
-
+secret_scanning_sample_token='secret_scanning_sample_token'
+console.log(`Using API_KEY \"${secret_scanning_sample_token}\"`)
