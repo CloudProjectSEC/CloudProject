07d07ed21edbb5d35f54b0eef58bff5e7953d892
Andre Hideo <onoda.hideo@gmail.com>
2025-04-01 23:35:18 -0400

    New test secret

--- index.js
+++ index.js
@@ -5,3 +5,5 @@ console.log('New feature is logging');
 console.log(`Using API_KEY \"${process.env.API_KEY}\"`)
 secret_scanning_sample_token='secret_scanning_sample_token'
 console.log(`Using API_KEY \"${secret_scanning_sample_token}\"`)
+AWS_SECRET_ACCESS_KEY='AKIA0987654321NEWKEY'
+console.log(`Using API_KEY \"${AWS_SECRET_ACCESS_KEY}\"`)
